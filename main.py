d9dcea46e0836f90bfa48b6e81a4863c7a843389
Bob Belderbos <bobbelderbos@gmail.com>
2022-06-14 20:40:28 +0200

    order the articles by publish date and some other small tweaks

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from dateutil.parser import parse
+
 from downloader import get_article_urls, download_articles
 from article_parser import parse_html_to_text, IN_DIR, OUT_DIR, ArticleException
 from pdf import create_pdf
 
 
+def _get_date(article):
+    lines = article.read_text().splitlines()
+    date = lines[2].removeprefix("Published: ")
+    return parse(date)
+
+
 def main():
+    # TODO: add Typer to run individual things, e.g. download, parse, pdf
     links = get_article_urls()
-    print(len(links))
+    print(f"{len(links)} articles retrieved from Pybites Article API")
 
     download_articles(links)
 
@@ -13,10 +22,16 @@ def main():
         try:
             parse_html_to_text(article_path)
         except ArticleException as exc:
-            print("ERROR", exc)
+            print(f"ERROR for {article_path}: {exc}")
 
-    articles = list(OUT_DIR.glob("*"))
-    create_pdf(articles)
+    articles = {
+        _get_date(article): article
+        for article in OUT_DIR.glob("*")
+    }
+    sorted_articles = [
+        article[1] for article in sorted(articles.items())
+    ]
+    create_pdf(sorted_articles)
 
 
 if __name__ == "__main__":
